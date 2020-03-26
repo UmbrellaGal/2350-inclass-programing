dc7b7f42a3486223c7087ef16f26336617c2c3a3
I shot myself again <hannahtaylor1@mail.weber.edu>
2020-03-26 11:26:40 -0600

    Final submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,43 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+import "bootstrap"
+
+const displayRandoBox = function(){
+    let r = Math.random()
+    if(r<.5){
+        document.write('<div class="red box"></div>')
+    }
+    else{
+        document.write('<div class="blue box"></div>')
+    }
+}
+
+function displayNBoxUsingFor(n){
+    for(let i=0; i<n; i++){
+        displayRandoBox()
+    }
+}
+function displayNBoxesUsingWhile(n){
+    let j=0;
+    while(j < n){
+        displayRandoBox()
+        j++
+    }
+}
+function displayNBoxesUsingDoWhile(n){
+    let k = 0;
+    do{
+    displayRandoBox()
+        k++
+    }while(k < n)
+}  
+
+displayNBoxUsingFor(14)
+displayNBoxesUsingWhile(14)
+displayNBoxesUsingDoWhile(14)
+
+const spuare = function(x){
+    return x*x
+}
+alert(square(10))
